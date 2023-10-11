0f7f6373817478f81ee5c75e0f1c2f27352fa0c0
sofiigorevna <sofia.konchencko@yandex.ru>
2023-10-11 19:56:20 +0300

    Initial Commit

---
+++ alomafire21-magicApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alomafire21-magicApi
+//
+//  Created by 1234 on 11.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
