758b88c8a94ebab3ed553b9490c2fc03b267c986
sofiigorevna <sofia.konchencko@yandex.ru>
2023-10-11 20:10:59 +0300

    Preparing the project for work.

---
+++ alomafire21-magicApi/Sources/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  alomafire21-magicApi
+//
+//  Created by 1234 on 11.10.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+         return true
+     }
+ }
